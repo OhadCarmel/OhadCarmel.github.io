e9112085c84e0f469927c82fc4bc36a1610a1696
ocarmel1 <ocarmel@gmail.com>
2024-07-13 11:31:25 +0300

    .

--- src/App.js
+++ src/App.js
@@ -1,25 +1,210 @@
-import logo from './logo.svg';
-import './App.css';
+// App.js
+import React, { useEffect, useRef, useState } from "react";
+// import BookListing from "./BookListing";
+// import BookDetails from "./BookDetails";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import axios from "axios";
+import { AppBar } from "@mui/material";
+// import FilterOptions from "./FilterOptions";
+import WelcomePage from "./Screens/WelcomePage/WelcomePage";
+import ExperimentScreen from "./Screens/ExperimentScreen/ExperimentScreen";
 
-function App() {
-  return (
+
+const App = () => {
+  axios.defaults.withCredentials = true;
+  const baseURL = "http://localhost:3080";
+
+  const [userId, setUserId] = useState([]);
+  const [books, setBooks] = useState([]);
+  const [startExperiment, setStartExperiment] = useState(false);
+  const [wordByWord, setWordByWord] = useState(false);
+  const [markHallucinations, setmarkHallucinations] = useState(false);
+  const [markLowConfidence, setmarkLowConfidence] = useState(false);
+  const [dbIndex, setDbIndex] = useState(0);
+  const [dab, setDab] = useState([]);
+  const imageIterations = useRef([]);
+
+  function handleStart() {
+    setStartExperiment(true);
+  }
+
+  function renderNewExperiment() {
+    setDbIndex((prevIndex) => prevIndex + 1);
+    console.log("Start Experiment");
+  }
+  useEffect(() => {
+    getUser();
+    getBooks();
+    getProbes();
+  }, []);
+
+  useEffect(()=> {
+    if(dbIndex===2){
+      console.log('sending');
+      console.log('imageIterations');
+      console.log(imageIterations.current); 
+      sendImageIterations();
+      setDbIndex(0)
+    } else {
+      console.log(`didn't send ${dbIndex}`);
+    }
+  },[dbIndex])
+  const getBooks = () => {
+    // axios
+    //   .get(`${baseURL}/books`)
+    //   .then((response) => setBooks(response.data.Books))
+    //   .catch((error) => console.error(error));
+  };
+
+  const getProbes = () => {
+    console.log('here');
+    axios
+      .get(`${baseURL}/random-docs`)
+      .then((response) => {
+        console.log('response')
+        console.log(response.data)
+        setDab(response.data)})
+      .catch((error) => console.error(error));
+  };
+
+  const handleSubmitMongo = async () => {
+    axios
+      .post(`${baseURL}/submit`)
+      .then(() => {
+        console.log("Data submitted successfully");
+      })
+      .catch((error) => {
+        console.error("Error submitting data:", error);
+      });
+  };
+
+  const handleFinishIteration = (imageIteration) => {
+    imageIterations.current.push({...imageIteration, userId});
+  };
+
+  const sendImageIterations = async () => {
+    axios
+      .post(`${baseURL}/submit-imageIteration`, {
+        imageIterations: {...imageIterations.current, userId: userId},   
+      })
+      .then(() => {
+        console.log("Data submitted successfully");
+      })
+      .catch((error) => {
+        console.error("Error submitting data:", error);
+      });};
+
+  const postBook = (title, author, publicationYear, description) => {
+    axios
+      .post(
+        `${baseURL}/books`,
+        {
+          book: { title, author, publicationYear, description },
+        },
+        {
+          headers: {
+            "content-type": "application/x-www-form-urlencoded",
+          },
+        }
+      )
+      .then((response) => {
+        setBooks([...books, response.data.book]);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+  const getUser = () => {
+    axios
+      .get(`${baseURL}/user`)
+      .then((response) => {
+
+        setUserId(response.data.id);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  const handleFilterChange = (publicationYear) => {
+    const params = [];
+    if (publicationYear) {
+      params.push(`after=${publicationYear[0]}&before=${publicationYear[1]}`);
+    }
+
+    let url = `${baseURL}/books${params ? `?${params.join("&")}` : ``}`;
+    axios
+      .get(url)
+      .then((response) => {
+        setBooks(response.data.Books);
+      })
+      .catch((error) => console.error(error));
+  };
+
+  const renderToolBar = () => {
+    return (
+      <AppBar position="sticky" color="inherit">
+        {/* <FilterOptions handleFilterChange={handleFilterChange} /> */}
+      </AppBar>
+    );
+  };
+
+  const renderNormal = () => (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {renderToolBar()}
+      <Router>
+        <Routes>
+          {/* <Route
+            path="/"
+            element={
+              // <BookListing
+              //   books={books}
+              //   postBook={postBook}
+              //   showTopReviewerBadge={
+              //     true // need to be changed the condition based on the instructions
+              //   }
+              // />
+            }
+          /> */}
+          {/* <Route path="/book/:bookId" element={<BookDetails />} /> */}
+        </Routes>
+      </Router>
     </div>
   );
-}
+
+  const renderExp = () =>
+    !startExperiment ? (
+      <WelcomePage
+        handleStart={handleStart}
+        handleWordByWord={setWordByWord}
+        wordByWord={wordByWord}
+        markHallucinations={markHallucinations}
+        handleMarkHallucinations={setmarkHallucinations}
+        markLowConfidence={markLowConfidence}
+        handleMarkLowConfidence={setmarkLowConfidence}
+      />
+    ) : (
+      <ExperimentScreen
+        firstProbe={dab[dbIndex].probes[0]}
+        secondProbe={dab[dbIndex].probes[1]}
+        description={
+          markHallucinations
+            ? dab[dbIndex].hallucinations
+            : dab[dbIndex].description
+        }
+        imgLink={dab[dbIndex].image_link}
+        renderNewExperiment={renderNewExperiment}
+        logits={dab[dbIndex].logits}
+        wordByWord={wordByWord}
+        markHallucinations={markHallucinations}
+        markLowConfidence={markLowConfidence}
+        sendImageIteration={handleFinishIteration}
+        wordsProb={dab[dbIndex].wordsProb}
+        sendImageIterations={sendImageIterations}
+      />
+    );
+
+  return renderExp();
+};
 
 export default App;
---
+++ src/Screens/WelcomePage/WelcomePage.js
@@ -0,0 +1,73 @@
+import React, { useState, component } from "react";
+import axios from "axios";
+import {
+  Typography,
+  Button,
+  Container,
+  Switch,
+  FormControlLabel,
+} from "@mui/material";
+import ExperimentScreen from "../ExperimentScreen/ExperimentScreen";
+
+function WelcomePage({
+  handleStart,
+  handleWordByWord,
+  wordByWord,
+  markHallucinations,
+  handleMarkHallucinations,
+  markLowConfidence,
+  handleMarkLowConfidence,
+}) {
+
+  
+  return (
+    <Container component="main" maxWidth="sm" sx={{ mt: 4 }}>
+      <Typography variant="h2" component="h1" gutterBottom>
+        Welcome to the Experiment
+      </Typography>
+      <Typography variant="body1" paragraph>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus.
+        Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies
+        sed, dolor. Cras elementum ultrices diam.
+      </Typography>
+      <FormControlLabel
+        control={
+          <Switch
+            checked={wordByWord}
+            onChange={(event) => handleWordByWord(event.target.checked)}
+            name="wordByWordSwitch"
+            color="primary"
+          />
+        }
+        label="Display Description Word by Word"
+      />
+      <FormControlLabel
+        control={
+          <Switch
+            checked={markHallucinations}
+            onChange={(event) => handleMarkHallucinations(event.target.checked)}
+            name="hallucinationsMarkedSwitch"
+            color="primary"
+          />
+        }
+        label="Display hallucinations marked"
+      />
+      <FormControlLabel
+        control={
+          <Switch
+            checked={markLowConfidence}
+            onChange={(event) => handleMarkLowConfidence(event.target.checked)}
+            name="lowConfidenceMarkedSwitch"
+            color="primary"
+          />
+        }
+        label="Display low confidence marked"
+      />
+      <Button variant="contained" color="primary" onClick={handleStart}>
+        Start
+      </Button>
+    </Container>
+  );
+}
+
+export default WelcomePage;
